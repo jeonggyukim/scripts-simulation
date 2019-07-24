cb8ed7476faaa900f4adc67739c3e966253e032c
Jeong-Gyu Kim <omyojj@gmail.com>
2019-07-24 17:58:53 -0400

    Add an option to rsync rst files. Add athena binary with different names.

--- move_output.py
+++ move_output.py
@@ -1,15 +1,17 @@
 #!/usr/bin/env python
-#!/usr/bin/e
+
 import glob
 import os
 import argparse
 
 basedir_orig_def = "/scratch/gpfs/jk11/TIGRESS-RT/R4_8pc.CR2.radp"
 basedir_new_def = "/tigress/jk11/TIGRESS-RT/R4_8pc.CR2.radp"
+join_vtk_def = False
+sync_rst_def = False
 
 parser = argparse.ArgumentParser(
-    description='''Move tigress simulation output files from gpfsto tigress using rsync. 
-To move vtk files, use join_vtk.sh script in Athena-TIGRESS/vtk''')
+    description='''Move tigress simulation output files from gpfsto tigress
+using rsync. To move vtk files, use vtk/join_vtk.sh script''')
 parser.add_argument('--basedir_orig', type=str,
                     default=basedir_orig_def,
                     help='original basedir')
@@ -17,7 +19,10 @@ parser.add_argument('--basedir_new', type=str,
                     default=basedir_new_def,
                     help='new basedir')
 parser.add_argument('--join_vtk',
-                    action='store_true', default=False,
+                    action='store_true', default=join_vtk_def,
+                    help='Toggle to join vtk files')
+parser.add_argument('--sync_rst',
+                    action='store_true', default=sync_rst_def,
                     help='Toggle to join vtk files')
 args = vars(parser.parse_args())
 locals().update(args)
@@ -31,18 +36,19 @@ if not os.path.isdir(basedir_orig):
     raise IOError('basedir_orig does not exist: ', basedir_orig)
 
 if os.path.isdir(basedir_new):
-    print('Directory {0:s} exists.'.format(basedir_new))
+    print('New basedir {0:s} exists.'.format(basedir_new))
 else:
-    print('Create directory {0:s}'.format(basedir_new))
+    print('Create new basedir {0:s}'.format(basedir_new))
     os.makedirs(basedir_new)
 
 basedir_new_hst = os.path.join(basedir_new, 'hst')
 basedir_new_star = os.path.join(basedir_new, 'starpar')
 basedir_new_zprof = os.path.join(basedir_new, 'zprof')
+basedir_new_rst = os.path.join(basedir_new, 'rst')
 basedir_new_vtk = os.path.join(basedir_new, 'vtk')
 
 for d in (basedir_new_hst, basedir_new_vtk, \
-          basedir_new_star, basedir_new_zprof):
+          basedir_new_star, basedir_new_zprof, basedir_new_rst):
     if not os.path.isdir(d):
         os.makedirs(d)
         print('Create directory {0:s}'.format(d))
@@ -55,12 +61,17 @@ rsync_zprof = 'rsync -av --include="*.zprof" --exclude="*" {0:s} {1:s}'.\
                                     format(basedir_orig_id0, basedir_new_zprof)
 rsync_star = 'rsync -av --include="*.starpar.vtk" --exclude="*" {0:s} {1:s}'.\
                                    format(basedir_orig_id0, basedir_new_star)
-rsync_misc = 'rsync -av  --include="athinput*" --include="athena" --include="*.txt" ' + \
+rsync_rst = 'rsync -av --include="*.rst" --exclude="*" {0:s}/id*/ {1:s}'.\
+                                   format(basedir_orig, basedir_new_rst)
+rsync_misc = 'rsync -av  --include="athinput*" --include="athena*" --include="radps_postproc*" --include="tigress*" --include="*.txt" ' + \
                                     '--exclude="*" {0:s} {1:s}'.\
                                     format(os.path.join(basedir_orig, ''), basedir_new)
 
 # do not rsync id0 directory
-commands = (rsync_hst, rsync_zprof, rsync_star, rsync_misc)
+commands = [rsync_hst, rsync_zprof, rsync_star, rsync_misc]
+if sync_rst:
+    commands.append(rsync_rst)
+
 for c in commands:
     print(c)
     os.system(c)
